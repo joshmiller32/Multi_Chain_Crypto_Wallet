4a74e6e7d520873113b40e58fdcc42907259709e
Christian Cathro <cscathro@gmail.com>
2019-12-23 19:02:26 -0600

    latest folder structure

--- backend/wallet.py
+++ backend/wallet.py
@@ -1,5 +1,12 @@
 from py_hd_wallet import wallet
-
+import sys
+'''
 def new_user():
     seed = wallet.generate_mnemonic()
     return seed
+    
+converting from function because electron can only read from system outputs
+'''
+seed = wallet.generate_mnemonic()
+print(seed)
+sys.stdout.flush()
